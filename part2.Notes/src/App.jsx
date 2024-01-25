50089a3272b641e1ac0eccb9b494a3e2bb574606
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2024-01-25 10:39:40 +0100

    add services and notes.js + import functions from noteService

--- part2.Notes/src/App.jsx
+++ part2.Notes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -8,10 +9,20 @@ const App = () => {
   const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/notes").then((response) => {
+    noteService.getAll().then((response) => {
       setNotes(response.data);
     });
   }, []);
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id !== id ? n : response.data)));
+    });
+  };
 
   const addNote = (event) => {
     event.preventDefault();
@@ -20,8 +31,7 @@ const App = () => {
       important: Math.random() < 0.5,
       // id: notes.length + 1, //removed so that the server can generate and id
     };
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      console.log(response);
+    noteService.create(noteObject).then((response) => {
       setNotes(notes.concat(response.data));
       setNewNote("");
     });
@@ -43,7 +53,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
