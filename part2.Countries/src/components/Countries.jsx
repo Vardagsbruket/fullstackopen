23e330f8457a7152a143211be86b144eb1f79767
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2024-02-06 13:00:33 +0100

    add more info abput countries if only one match with search

--- part2.Countries/src/components/Countries.jsx
+++ part2.Countries/src/components/Countries.jsx
@@ -2,6 +2,31 @@ const Countries = ({ filteredCountries, searchValue }) => {
   if (searchValue === null || searchValue === "") {
     return null;
   }
+
+  if (filteredCountries.length === 1) {
+    const country = filteredCountries[0];
+    const languages = Object.entries(country.languages).map(([code, name]) => (
+      <li key={code}>{name}</li>
+    ));
+    const flag = <img src={country.flags.png} alt={country.flags.alt} />;
+    return (
+      <>
+        <div>
+          <h2>{country.name.common}</h2>
+          <p>Capital: {country.capital}</p>
+          <p>Area: {country.area}</p>
+          <h3>Languages:</h3>
+          <ul>{languages}</ul>
+          {flag}
+        </div>
+      </>
+    );
+  }
+
+  if (filteredCountries.length > 10) {
+    return null; // Return null if more than 10 countries
+  }
+
   return (
     <>
       <ul>
