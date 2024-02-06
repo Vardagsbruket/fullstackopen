9abc3118b2a3ab6edc839566cd756ba5ca686815
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2024-02-06 13:23:42 +0100

    add button to show information about country in the result list

--- part2.Countries/src/components/Countries.jsx
+++ part2.Countries/src/components/Countries.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Countries = ({ filteredCountries, searchValue }) => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
   if (searchValue === null || searchValue === "") {
     return null;
   }
@@ -27,13 +31,36 @@ const Countries = ({ filteredCountries, searchValue }) => {
     return null; // Return null if more than 10 countries
   }
 
+  const handleShowCountry = (country) => {
+    setSelectedCountry(country);
+  };
+
   return (
     <>
       <ul>
         {filteredCountries.map((country) => (
-          <li key={country.alpha3code}>{country.name.common}</li>
+          <li key={country.alpha3code}>
+            {country.name.common}{" "}
+            <button onClick={() => handleShowCountry(country)}>show</button>{" "}
+          </li>
         ))}
       </ul>
+      {selectedCountry && (
+        <div>
+          <h2>{selectedCountry.name.common}</h2>
+          <p>Capital: {selectedCountry.capital}</p>
+          <p>Area: {selectedCountry.area}</p>
+          <h3>Languages:</h3>
+          <ul>
+            {Object.entries(selectedCountry.languages).map(
+              ([code, language]) => (
+                <li key={code}>{language}</li>
+              )
+            )}
+          </ul>
+          <img src={selectedCountry.flags.png} alt="Flag" />
+        </div>
+      )}
     </>
   );
 };
